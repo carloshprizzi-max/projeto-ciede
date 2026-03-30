09f49b61e3d634f945f792919a620e80cf2a3720
Carlos Rizzi <carloshprizzi@gmail.com>
2026-03-30 10:30:55 -0300

    ajuste porta render

--- backend/server.js
+++ backend/server.js
@@ -85,6 +85,12 @@ app.delete("/mensagens/:id", (req, res) => {
 });
 
 // iniciar servidor
-app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
+// app.listen(3000, () => {
+//   console.log("Servidor rodando em http://localhost:3000");
+// });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando...");
 });
+
