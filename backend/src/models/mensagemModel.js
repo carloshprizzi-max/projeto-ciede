bb0e55f533193627a0cb1e38bc9503e8c91ff716
Carlos Rizzi <carloshprizzi@gmail.com>
2026-04-01 22:33:47 -0300

    fix: corrigir coluna data

--- backend/src/models/mensagemModel.js
+++ backend/src/models/mensagemModel.js
@@ -16,18 +16,30 @@ exports.create = (nome, email, mensagem) => {
 
 exports.getAll = () => {
   return new Promise((resolve, reject) => {
-     db.all(
-     `
-      SELECT id, nome, email, mensagem, data_envio as data
-      FROM mensagens
-      ORDER BY data_envio DESC
-     `,
-     [],
-     (err, rows) => {
-        if (err) reject(err);
-        else resolve(rows);
-      }
-    );
+    //  db.all(
+    //  `
+    //   SELECT id, nome, email, mensagem, data_envio as data
+    //   FROM mensagens
+    //   ORDER BY data_envio DESC
+    //  `,
+    //  [],
+    //  (err, rows) => {
+    //     if (err) reject(err);
+    //     else resolve(rows);
+    //   }
+    // );
+    db.all(
+  "SELECT * FROM mensagens ORDER BY id DESC",
+  [],
+  (err, rows) => {
+    if (err) {
+      console.error("ERRO SQL:", err.message);
+      reject(err);
+    } else {
+      resolve(rows);
+    }
+  }
+);
   });
 }
 
