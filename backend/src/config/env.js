393c135ef23d2d11bd873ac621044ec679152543
Carlos Rizzi <carloshprizzi@gmail.com>
2026-04-01 21:52:27 -0300

    fix: garantir carregamento env

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+console.log("JWT_SECRET carregado:", process.env.JWT_SECRET);
+
 module.exports = {
   jwtSecret: process.env.JWT_SECRET,
   adminEmail: process.env.ADMIN_EMAIL,
