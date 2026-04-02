fecbf80335074fa0036425972eaaedf4f84a2121
Carlos Rizzi <carloshprizzi@gmail.com>
2026-04-01 22:42:31 -0300

    ajuste data

--- backend/src/models/mensagemModel.js
+++ backend/src/models/mensagemModel.js
@@ -16,32 +16,20 @@ exports.create = (nome, email, mensagem) => {
 
 exports.getAll = () => {
   return new Promise((resolve, reject) => {
-    //  db.all(
-    //  `
-    //   SELECT id, nome, email, mensagem, data_envio as data
-    //   FROM mensagens
-    //   ORDER BY data_envio DESC
-    //  `,
-    //  [],
-    //  (err, rows) => {
-    //     if (err) reject(err);
-    //     else resolve(rows);
-    //   }
-    // );
-    db.all(
-  "SELECT * FROM mensagens ORDER BY id DESC",
-  [],
-  (err, rows) => {
-    if (err) {
-      console.error("ERRO SQL:", err.message);
-      reject(err);
-    } else {
-      resolve(rows);
-    }
-  }
-);
+     db.all(
+     `
+      SELECT id, nome, email, mensagem, data
+      FROM mensagens
+      ORDER BY data DESC
+     `,
+     [],
+     (err, rows) => {
+        if (err) reject(err);
+        else resolve(rows);
+      }
+    );
   });
-}
+};
 
 exports.deleteById = (id) => {
   return new Promise((resolve, reject) => {
