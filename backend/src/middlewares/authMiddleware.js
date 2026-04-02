957dffb4f68631ccc0462075c64f07b0de350fec
Carlos Rizzi <carloshprizzi@gmail.com>
2026-04-01 21:12:47 -0300

    loginhtml

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -4,17 +4,23 @@ const env = require("../config/env");
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
+  console.log("HEADER:", authHeader);
+
   if (!authHeader) {
     return res.status(401).json({ erro: "Token não enviado" });
   }
 
   const token = authHeader.split(" ")[1];
 
+  console.log("TOKEN:", token);
+  console.log("SECRET:", env.jwtSecret);
+
   try {
     const decoded = jwt.verify(token, env.jwtSecret);
     req.user = decoded;
     next();
-  } catch {
+  } catch (err) {
+    console.log("ERRO JWT:", err.message);
     return res.status(403).json({ erro: "Token inválido" });
   }
 };
--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -1,23 +1,8 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 const env = require("../config/env");
 
-const gerarHash = async () => {
-  return await bcrypt.hash(env.adminPassword, 10);
-};
-
-// vamos gerar hash uma vez
-let hashedPassword;
-gerarHash().then(hash => hashedPassword = hash);
-
 exports.login = async (email, senha) => {
-  if (email !== env.adminEmail) {
-    throw new Error("Credenciais inválidas");
-  }
-
-  const senhaValida = await bcrypt.compare(senha, hashedPassword);
-
-  if (!senhaValida) {
+  if (email !== env.adminEmail || senha !== env.adminPassword) {
     throw new Error("Credenciais inválidas");
   }
 
